796db1d42dc9f1bc608671208d9d33a2990b9d3d
panos1962 <panos1962@localhost>
2014-07-29 19:45:28 +0300

    modifications

--- skiser/service/trparam.js
+++ skiser/service/trparam.js
@@ -29,7 +29,7 @@ Service.trparam.set = function(nodereq) {
 };
 
 Service.trparam.set2 = function(nodereq, trapezi) {
-	var kinisi;
+	var kinisi, post;
 
 	nodereq.end();
 	kinisi = new Kinisi({
@@ -43,5 +43,95 @@ Service.trparam.set2 = function(nodereq, trapezi) {
 	});
 	if (nodereq.url.apodoxi) kinisi.data.apodoxi = 1;
 
-	Server.skiniko.processKinisi(kinisi).kinisiAdd(kinisi);
+	Server.skiniko.
+	processKinisi(kinisi).
+	kinisiAdd(kinisi);
+
+	post = Service.trparam['post' + nodereq.url.param];
+	if (typeof post === 'function') post(kinisi.data);
+};
+
+// Στην περίπτωση που το τραπέζι γίνεται πριβέ θα πρέπει να αποβληθούν όλοι
+// οι θεατές που δεν έχουν πρόσκληση.
+
+Service.trparam.postΠΡΙΒΕ = function(data) {
+	var skiniko, trapezi, apovoli;
+
+	// Αν το τραπέζι γίνεται δημόσιο δεν προβαίνουμε σε περαιτέρω
+	// ενέργειες.
+
+	if (data.timi.isOxi()) return;
+
+	skiniko = Server.skiniko;
+	trapezi = skiniko.skinikoTrapeziGet(data.trapezi);
+	if (!trapezi) return;
+
+	// Θα μαζέψουμε σε λίστα τους θεατές του τραπεζιού που δεν έχουν
+	// πρόσκληση με σκοπό να τους αποβάλουμε από το τραπέζι.
+
+	apovoli = {};
+	skiniko.skinikoSinedriaWalk(function() {
+		var pektis;
+
+		if (this.sinedriaOxiTrapezi(data.trapezi)) return;
+		if (this.sinedriaOxiTheatis()) return;
+
+		pektis = this.sinedriaPektisGet();
+		if (trapezi.trapeziIsProsklisi(pektis)) return;
+
+		apovoli[pektis] = true;
+	});
+
+	// Εκκινούμε τη διαδικασία αποβολής.
+
+	Service.trparam.exoTheatis(apovoli);
+
+};
+
+// Η διαδικασία αποβολής λειτουργεί αλυσιδωτά αποβάλλοντας κάθε φορά τον πρώτο
+// παίκτη που υπάρχει στη λίστα, διαγράφοντάς τον από τη λίστα και εκτελώντας
+// αναδρομή στην υπόλοιπη λίστα μέχρι η λίστα να εξαντληθεί.
+
+Service.trparam.exoTheatis = function(apovoli) {
+	var conn, login, query;
+
+	for (login in apovoli) {
+		delete apovoli[login];
+
+		conn = DB.connection();
+		query = 'UPDATE `sinedria` SET `trapezi` = NULL, `thesi` = NULL, `simetoxi` = NULL ' +
+			'WHERE `pektis` = ' + login.json();
+		conn.connection.query(query, function(err, res) {
+			var kinisi;
+
+			conn.free();
+
+			// Αν κάτι δεν πήγε καλά, αφήνω τον θεατή στο κλειδωμένο
+			// τραπέζι ώστε οι κλειδοκράτορες να γνωρίζουν ότι αυτός
+			// παραμένει.
+
+			if ((!res) || (res.affectedRows < 1))
+			return;
+
+			// Αλλιώς προβαίνουμε σε δημιουργία κίνησης εξόδου του
+			// συγκεκριμένου παίκτη από το τραπέζι.
+
+			kinisi = new Kinisi({
+				idos: 'RT',
+				data: {
+					pektis: login,
+				},
+			});
+
+			Server.skiniko.
+			processKinisi(kinisi).
+			kinisiAdd(kinisi);
+		});
+
+		// Έχουμε τελειώσει με τον πρώτο παίκτη της λίστας και καλούμε
+		// αναδρομικά με την υπόλοιπη λίστα.
+
+		Service.trparam.exoTheatis(apovoli);
+		return;
+	}
 };
