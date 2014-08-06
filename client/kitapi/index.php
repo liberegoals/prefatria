e92aa82aec86921726538a5f013ddaa383d88a4f
panos1962 <panos1962@gmail.com>
2014-08-06 21:54:34 +0300

    modifications

--- client/kitapi/index.php
+++ client/kitapi/index.php
@@ -9,7 +9,6 @@ Selida::javascript("kitapi/kitapi");
 Selida::body();
 Selida::ofelimo_begin();
 Kitapi::setup();
-//Kitapi::enimerosi();
 Selida::ofelimo_end();
 Selida::telos();
 
@@ -27,25 +26,6 @@ class Kitapi {
 		</tr>
 		</table>
 		<div id="kitapiPerioxi1" class="kitapiPerioxi"></div>
-		<?php
-	}
-
-	public static function enimerosi() {
-		?>
-		<div id="kitapiTabela">
-		<div class="kitapiTabelaParagrafos">
-		Φίλοι πρεφαδόροι,
-		<br />
-		Το κιτάπι του νέου «Πρεφαδόρου» θα είναι σύντομα κοντά σας!
-		</div>
-		<div class="kitapiTabelaParagrafos">
-		Ως τότε, απλώς σας υπενθυμίζω ότι το κιτάπι δεν παρέχει καμια χρήσιμη πληροφορία,
-		ούτε θα πρέπει με οποιονδήποτε τρόπο να επηρεάζει το παίξιμο και τις αγορές σας.
-		Πράγματι, τα καπίκια που έχουν απομείνει στην κάσα ανήκουν εξίσου και στους τρεις
-		παίκτες, ενώ τα κέρδη και οι ζημίες των παικτών στην τρέχουσα φάση του παιχνιδιού
-		εμφανίζονται ευκρινώς στις αντίστοιχες περιοχές.
-		</div>
-		</div>
 		<?php
 	}
 }
