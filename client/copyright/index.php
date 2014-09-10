5ac933f206a9f8fce0ad482250708b76f181a3e2
panos1962 <panos1962@gmail.com>
2014-09-10 17:20:01 +0300

    modifications

---
+++ client/copyright/index.php
@@ -0,0 +1,19 @@
+<?php
+require_once "../lib/selida.php";
+
+Selida::head();
+Selida::stylesheet("copyright/copyright");
+Selida::javascript("copyright/copyright");
+
+Selida::body();
+Selida::toolbar();
+Selida::fyi_pano();
+
+Selida::ofelimo_begin();
+require "copyright.html";
+Selida::ofelimo_end();
+
+Selida::fyi_kato();
+Selida::ribbon();
+Selida::telos();
+?>
