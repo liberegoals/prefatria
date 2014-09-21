3e456cee7e0f268dd99057d14ff63469dedaa7dd
panos1962 <panos1962@gmail.com>
2014-09-21 21:44:49 +0300

    modifications

---
+++ client/minima/index.php
@@ -0,0 +1,73 @@
+<?php
+require_once "../lib/standard.php";
+Globals::diavase("lib/selida.php");
+
+Selida::head("Αλληλογραφία");
+Globals::pektis_must();
+Minima::pektisSet();
+Globals::database();
+
+Selida::stylesheet("minima/minima");
+Selida::javascript("minima/minima");
+Minima::setupView();
+
+Selida::body();
+Selida::toolbar();
+Selida::fyi_pano();
+
+Selida::ofelimo_begin();
+Minima::epikefalida();
+Minima::minimata();
+Minima::klisimo();
+Selida::ofelimo_end();
+
+Selida::fyi_kato();
+Selida::ribbon();
+Selida::telos();
+
+Class Minima {
+	private static $pektis;
+
+	public static function setupView() {
+		?>
+		<?php
+	}
+
+	public static function pektisSet() {
+		self::$pektis = $_SESSION["pektis"];
+	}
+
+	public static function epikefalida() {
+		?>
+		<table id="minimata" style="width: 100%;">
+		<?php
+	}
+
+	public static function klisimo() {
+		?>
+		</table>
+		<?php
+	}
+
+	public static function minimata() {
+		$query = "SELECT `kodikos`, `pote`, `apostoleas`, `paraliptis`, `kimeno` " .
+			"FROM `minima` WHERE " .
+			"(`apostoleas` = " . Globals::asfales_sql(self::$pektis) . ") OR " .
+			"(`paraliptis` = " . Globals::asfales_sql(self::$pektis) . ") " .
+			"ORDER BY `kodikos`";
+		$result = Globals::query($query);
+		while ($row = $result->fetch_array(MYSQL_NUM)) {
+			?>
+			<tr>
+			<td class="minimaKodikos"><?php print $row[0]; ?></td>
+			<td class="minimaImerominia"><?php print $row[1]; ?></td>
+			<td class="minimaPios"><?php print $row[2]; ?></td>
+			<td class="minimaMinima"><?php print $row[4]; ?></td>
+			<td class="minimaPanel"></td>
+			</tr>
+			<?php
+		}
+		$result->free();
+	}
+}
+?>
