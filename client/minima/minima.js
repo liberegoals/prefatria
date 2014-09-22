54a52670c0df19a48a1f56cd4e8348007b76bf83
panos1962 <panos1962@gmail.com>
2014-09-22 15:31:54 +0300

    modifications

--- client/minima/minima.js
+++ client/minima/minima.js
@@ -17,10 +17,51 @@ Minima.setupMinimata = function() {
 	append($('<img>').addClass('minimaPanelIcon').attr({
 		src: '../ikona/minima/diagrafi.png',
 		title: 'Διαγραφή μηνύματος',
+	}).
+	on('click', function(e) {
+		var minimaDOM, kodikos;
+
+		minimaDOM = $(this).parents('.minima');
+		kodikos = minimaDOM.find('.minimaKodikos').text();
+		if (!kodikos) return;
+
+		Client.fyi.pano('Διαγραφή μηνύματος. Παρακαλώ περιμένετε…');
+		Client.skiserService('minimaDelete', 'minima=' + kodikos).
+		done(function(rsp) {
+			Client.fyi.pano(rsp);
+			minimaDOM.addClass('minimaDiagrafi').
+			fadeOut(function() {
+				$(this).remove();
+			});
+		}).
+		fail(function(err) {
+			Client.skiserFail(err);
+		});
 	}))).
 	append($('<div>').addClass('minimaPanelButton').
 	append($('<img>').addClass('minimaPanelIcon').attr({
 		src: '../ikona/minima/diavasmeno.png',
+	}).
+	on('click', function(e) {
+		var minimaDOM, kodikos;
+
+		minimaDOM = $(this).parents('.minima');
+		kodikos = minimaDOM.find('.minimaKodikos').text();
+		if (!kodikos) return;
+
+		Client.fyi.pano('Αλλαγή κατάστασης μηνύματος. Παρακαλώ περιμένετε…');
+		Client.skiserService('minimaDiavasma', 'minima=' + kodikos).
+		done(function(rsp) {
+			Client.fyi.pano(rsp);
+			if (rsp === 'ΔΙΑΒΑΣΜΕΝΟ')
+			minimaDOM.addClass('minimaDiavasmeno').removeClass('minimaTrexon');
+
+			else
+			minimaDOM.addClass('minimaTrexon').removeClass('minimaDiavasmeno');
+		}).
+		fail(function(err) {
+			Client.skiserFail(err);
+		});
 	}))).
 	append($('<div>').addClass('minimaPanelButton').
 	append($('<img>').addClass('minimaPanelIcon').attr({
@@ -29,12 +70,14 @@ Minima.setupMinimata = function() {
 
 	Minima.minimataDOM.
 	on('mouseenter', '.minima', function(e) {
-		$(this).addClass('minimaTrexon').
-		children('.minimaPios').attr('title', 'Απάντηση');
+		$(this).addClass('minimaTrexon');
+		$(this).children('.minimaPios').attr('title', 'Απάντηση');
+		$(this).children('.minimaPanel').finish().fadeTo(100, 1);
 	}).
 	on('mouseleave', '.minima', function(e) {
-		$(this).removeClass('minimaTrexon').
-		children('.minimaPios').removeAttr('title');
+		$(this).removeClass('minimaTrexon');
+		$(this).children('.minimaPios').removeAttr('title');
+		$(this).children('.minimaPanel').finish().fadeTo(100, 0);
 	}).
 	on('click', '.minima', function(e) {
 		//Minima.editFormaDOM.finish().fadeIn(100);
