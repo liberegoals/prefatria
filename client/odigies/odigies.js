1bef5b780635df95a2b9b17a26cd182efbe4b8c6
panos1962 <panos1962@gmail.com>
2014-09-26 14:32:27 +0300

    modifications

---
+++ client/odigies/odigies.js
@@ -0,0 +1,31 @@
+
+/*
+	public static function odigies() {
+Page::epikefalida(Globals::perastike('pedi'), '<div id="tutorialVideos" ' .
+	'class="odigiesTutorialVideos">[&nbsp;<a target="_self" ' .
+	'href="http://www.youtube.com/playlist?list=PL07F1583FEB191C3D&amp;feature=mh_lolz">' .
+	'Εκπαιδευτικά βίντεο</a>&nbsp;]</div>');
+?>
+<script type="text/javascript">
+//<![CDATA[
+function kouniseTutorialVideo(div, i) {
+	if (notSet(div)) { return; }
+	if (i > 10) {
+		div.setAttribute('class', 'odigiesTutorialVideos');
+		return;
+	}
+
+	div.setAttribute('class', 'odigiesTutorialVideos' + (i % 2));
+	setTimeout(function() {
+		kouniseTutorialVideo(div, i + 1);
+	}, 100);
+}
+*/
+
+$(document).ready(function() {
+	$('#enimerotiko').append(Client.klisimo()).siromeno({
+		top: '20px',
+		right: '20px',
+	});
+	//kouniseTutorialVideo(getelid('tutorialVideos'), 0);
+});
