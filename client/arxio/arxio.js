73985ac81261e2b27becf13f758dd7628b6edeab
panos1962 <panos1962@gmail.com>
2014-10-10 12:17:54 +0300

    modifications

--- client/arxio/arxio.js
+++ client/arxio/arxio.js
@@ -3,6 +3,7 @@ Arxio = {};
 $(document).ready(function() {
 	Client.tabPektis();
 	Client.tabKlisimo($('#toolbarRight'));
+	Arxio.setup();
 
 	Arena = null;
 	if (!window.opener) return;
@@ -26,3 +27,26 @@ on('beforeunload', function() {
 on('unload', function() {
 	Arxio.unload();
 });
+
+Arxio.setup = function() {
+	Client.ofelimoDOM.
+	append(Arxio.kritiriaDOM = $('<div>').attr('id', 'kritiria')).
+	append(Arxio.apotelesmataDOM = $('<div>').attr('id', 'apotelesmata'));
+
+	Arxio.kritiriaSetup();
+};
+
+Arxio.kritiriaSetup = function() {
+	Arxio.kritiriaDOM.
+	append($('<div>').addClass('formaPrompt').text('Παίκτης')).
+	append(Arxio.pektisInputDOM = $('<input>').addClass('formaPedio').css('width', '140px')).
+	append($('<div>').addClass('formaPrompt').text('Από')).
+	append(Arxio.apoInputDOM = Client.inputDate()).
+	append($('<div>').addClass('formaPrompt').text('Έως')).
+	append(Arxio.eosInputDOM = Client.inputDate()).
+	append($('<div>').addClass('formaPrompt').text('Παρτίδα')).
+	append(Arxio.partidaInputDOM = $('<input>').addClass('formaPedio').css('width', '70px')).
+	append($('<button>').addClass('formaButton').text('Go!!!')).
+	append($('<button>').addClass('formaButton').text('<')).
+	append($('<button>').addClass('formaButton').text('>'));
+};
