9d0a9b5182ea57f4cfd046b7668536e293540780
panos1962 <panos1962@gmail.com>
2014-10-11 18:27:06 +0300

    modifications

--- client/arxio/arxio.js
+++ client/arxio/arxio.js
@@ -61,13 +61,13 @@ Arxio.kritiriaSetup = function() {
 
 	date = new Date();
 	mera = date.getDate();
-	minas = date.getMonth();
+	minas = date.getMonth() + 1;
 	etos = date.getFullYear();
 	Arxio.eosInputDOM.val(mera + '-' + minas + '-' + etos);
 
 	date = new Date(date.getTime() - (7 * 24 * 3600 * 1000));
 	mera = date.getDate();
-	minas = date.getMonth();
+	minas = date.getMonth() + 1;
 	etos = date.getFullYear();
 	Arxio.apoInputDOM.val(mera + '-' + minas + '-' + etos);
 
@@ -162,29 +162,64 @@ Trapezi.prototype.trapeziArxioKapikia = function() {
 };
 
 Trapezi.prototype.trapeziArxioDisplay = function() {
-	var trapezi = this, kodikos, dom;
+	var trapezi = this, kodikos;
+
+	if (this.DOM) this.DOM.emtpy();
+	else Arxio.apotelesmataDOM.append(this.DOM = $('<div>').addClass('trapeziTsoxa'));
 
 	kodikos = this.trapeziKodikosGet();
 
-	Arxio.apotelesmataDOM.
-	append(dom = $('<div>').addClass('trapeziTsoxa'));
-
-	dom.append($('<div>').addClass('trapeziData').
+	this.DOM.
+	append($('<div>').addClass('trapeziData').
 	append($('<div>').addClass('trapeziDataKodikos').text(kodikos)).
 	append($('<div>').addClass('trapeziDataIpolipo').text(this.ipolipo)));
 	Prefadoros.thesiWalk(function(thesi) {
-		var pektis;
+		var pektis, dom, kapikia, kapikiaKlasi;
 
 		pektis = trapezi.trapeziPektisGet(thesi);
 		if (!pektis) pektis = '&#8203;';
-		dom.append($('<div>').addClass('pektis trapeziPektis').html(pektis));
+		trapezi.DOM.append(dom = $('<div>').addClass('pektis trapeziPektis').html(pektis));
+
+		kapikia = parseInt(trapezi['kapikia' + thesi]);
+		if (isNaN(kapikia)) kapikia = 0;
+		if (!kapikia) return;
+
+		kapikiaKlasi = 'arxioKapikia';
+		if (kapikia < 0) kapikiaKlasi += ' arxioKapikiaMion';
+
+		dom.append($('<div>').addClass(kapikiaKlasi).text(kapikia));
 	});
 
 	arxio = trapezi.trapeziArxioGet();
-	if (arxio) 
-	dom.append($('<div>').addClass('trapeziArxio').text(Globals.poteOra(arxio)));
-	else
-	dom.append($('<div>').addClass('trapeziArxio plagia').text('Σε εξέλιξη…'));
+	if (arxio) this.DOM.append($('<div>').addClass('trapeziArxio').text(Globals.poteOra(arxio)));
+	else this.DOM.append($('<div>').addClass('trapeziArxio plagia').text('Σε εξέλιξη…'));
 
+	this.trapeziArxioOptions();
+	return this;
+};
+
+Trapezi.prototype.trapeziArxioOptions = function() {
+	this.DOM.
+	append(this.optsDOM = $('<div>').addClass('trapeziOpts'));
+
+	if (this.trapeziIsPaso()) this.trapeziOptionIcon('Παίζεται το πάσο', 'pasoOn.png');
+	if (this.trapeziOxiAsoi()) this.trapeziOptionIcon('Δεν παίζονται οι άσοι', 'asoiOn.png');
+	if (this.trapeziTeliomaAnisoropo())
+	this.trapeziOptionIcon('Ανισόρροπη πληρωμή τελευταίας αγοράς', 'postel/anisoropo.png');
+	else if (this.trapeziTeliomaDikeo())
+	this.trapeziOptionIcon('Δίκαιη πληρωμή τελευταίας αγοράς', 'postel/dikeo.png');
+	if (this.trapeziIsFiliki()) this.trapeziOptionIcon('Εκπαιδευτική/Φιλική παρτίδα', 'filiki.png');
+	if (this.trapeziIsKlisto()) this.trapeziOptionIcon('Κλειστό τραπέζι', 'klisto.png');
+	if (this.trapeziIsPrive()) this.trapeziOptionIcon('Πριβέ τραπέζι', 'prive.png');
+	if (this.trapeziIsIdioktito()) this.trapeziOptionIcon('Ιδιόκτητο τραπέζι',
+		this.trapeziThesiPekti(Client.session.pektis) === 1 ? 'elefthero.png' : 'idioktito.png');
+	return this;
+};
+
+Trapezi.prototype.trapeziOptionIcon = function(desc, img) {
+	this.optsDOM.append($('<img>').addClass('trapeziOption').attr({
+		title: desc,
+		src: '../ikona/panel/' + img,
+	}));
 	return this;
 };
