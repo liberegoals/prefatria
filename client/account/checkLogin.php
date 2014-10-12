9871bcc12e2149391f7f75dd93ec7a92d97cdd28
panos1962 <panos1962@gmail.com>
2014-10-12 14:08:16 +0300

    modifications

--- client/account/checkLogin.php
+++ client/account/checkLogin.php
@@ -4,3 +4,4 @@ Globals::database();
 $query = "SELECT `login` FROM `pektis` WHERE `login` = " . Globals::asfales_sql($_REQUEST["login"]);
 $res = @Globals::$db->query($query);
 if ($res->num_rows) print "X";
+?>
