eb38eb84854aa874b2c4f900c6cfdfc724af229f
panos1962 <panos1962@gmail.com>
2014-10-16 17:08:02 +0300

    modifications

---
+++ client/depo/pep/index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="Το πλήρες εγχειρίδιο της πρέφας" />
+<meta name="author" content="Panos I. Papadopoulos" />
+<meta name="copyright" content="Copyright by Panos Papadopoulos. All Rights Reserved." />
+<title>Το πλήρες εγχειρίδιο της πρέφας</title>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/themes/smoothness/jquery-ui.css" />
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"></script>
+<link rel="stylesheet" href="standard.css" />
+<script type="text/javascript" src="standard.js"></script>
+</head>
+<body>
+<img src="fonto.png" />
+<div id="kimeno1" class="kimeno">
+	Όλα όσα θέλετε να μάθετε για την πρέφα!
+</div>
+<div id="kimeno2" class="kimeno">
+	Το Πλήρες Εγχειρίδιο της Πρέφας
+</div>
+<div id="kimeno3" class="kimeno">
+	Ένα βιβλίο των <span class="sigrafeas">Δημήτρη Κούρτη</span>
+	και <span class="sigrafeas">Σπύρου Τσοβόλα</span>
+</div>
+<div id="kimeno4" class="kimeno">
+	Παραδοτέο στο χώρο σας σε λίγες ημέρες
+	<br />
+	από τις
+	<a target="_blank" href="http://www.kapsimi.gr/plires-egxeiridio-tis-prefas">εκδόσεις ΚΨΜ</a>,
+	ή μέσω του
+	<a target="_blank" href="http://www.bridgedeal.gr/p/blog-page_7338.html">www.bridgedeal.gr</a>
+</div>
+</body>
+</html>
