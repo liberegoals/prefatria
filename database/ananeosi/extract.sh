2d0df16a71b55cc5fd0d89107ee7f42302d000ac
panos1962 <panos1962@gmail.com>
2014-10-27 17:16:18 +0200

    modifications

--- database/ananeosi/extract.sh
+++ database/ananeosi/extract.sh
@@ -2,9 +2,7 @@
 
 data_files="pektis.data peparam.data profinfo.data sxesi.data isfora.data"
 
-(cd /tmp; rm -f ${data_files}) || exit 2
 mysql -u prefadoros -p <extract.sql || exit 2
 
 cd /tmp
 tar czf data.tar.gz ${data_files} || exit 2
-rm -f ${data_files} || exit 2
