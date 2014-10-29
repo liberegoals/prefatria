32f92769303453b479ea2fe064e7ea32dc03b963
panos1962 <panos1962@gmail.com>
2014-10-29 11:41:27 +0200

    modifications

---
+++ client/funchat/index.php
@@ -0,0 +1,20 @@
+<?php
+require_once "../lib/selida.php";
+
+Selida::head("Πρεφαδόρος - Funchat");
+Selida::stylesheet("funchat/funchat");
+Selida::javascript("common/prefadoros");
+Selida::javascript("funchat/common");
+Selida::javascript("funchat/funchat");
+
+Selida::body();
+Selida::ofelimo_begin();
+Funchat::setup();
+Selida::ofelimo_end();
+Selida::telos();
+
+class Funchat {
+	public static function setup() {
+	}
+}
+?>
