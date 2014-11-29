1c0881f32af4a96a5631f8734d8db3ca4f83a61e
panos1962 <panos1962@localhost>
2014-11-29 23:36:17 +0200

    modifications

---
+++ client/depo/kelsos/index.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="Μουσικό σύνολο «Κέλσος»" />
+<meta name="author" content="Panos I. Papadopoulos" />
+<meta name="copyright" content="Copyright by Panos Papadopoulos. All Rights Reserved." />
+<title>Μουσικό σύνολο «Κέλσος»</title>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/themes/smoothness/jquery-ui.css" />
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js"></script>
+<link rel="stylesheet" href="standard.css" />
+<script type="text/javascript" src="standard.js"></script>
+</head>
+<body>
+<a target="diafimisi" href="http://www.kelsos.gr/index.php?lang=el">
+<div id="plesio"></div>
+<img src="fonto.png" />
+
+<div id="kimeno1" class="kimeno">
+Μουσικό σύνολο «Κέλσος»
+</div>
+
+<div id="kimeno2" class="kimeno">
+Μουσικές γέφυρες στο πέρασμα επτά αιώνων
+</div>
+
+<div id="kimeno3" class="kimeno">
+Μια περιπλάνηση στην Ευρώπη με τραγούδια και χορούς απο τον Μεσαίωνα,
+φωνητικές και οργανικές συνθέσεις απο την Αναγέννηση και το Μπαρόκ,
+μπαλάντες, βιλανέλλες, τσιακώνες, πασσακάλιες και ταραντέλλες
+απο την ανώνυμη παράδοση του 17ου και 18ου αιώνα,
+και τραγούδια του σήμερα σε πρωτότυπες διασκευές για όργανα εποχής.
+</div>
+
+<div id="kimeno4" class="kimeno">
+ 
+Την Τετάρτη 3 &amp; 10 Δεκεμβρίου 2014
+<br />
+στο "Αγγέλων Βήμα", Σατωβριάνδου 36 (Μετρό Ομόνοια)
+<br />
+ώρα 7μμ, είσοδος 10 ευρώ
+<br />
+<hr />
+Πληροφορίες: 210-5242211
+</div>
+
+</a>
+</body>
+</html>
