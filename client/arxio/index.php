a33bf2f06eb9199657e4b543a5955ea11b4c1b6b
panos1962 <panos1962@localhost>
2014-12-27 15:00:22 +0200

    modifications

--- client/arxio/index.php
+++ client/arxio/index.php
@@ -30,74 +30,63 @@ class Arxio {
 	// και τα κρατάμε ως data στα αντίστοιχα πεδία για το reset.
 
 	public static function kritiria() {
+		// Εάν έχουν δοθεί κριτήρια στο URL, τότε προτιθέμεθα να προχωρήσουμε
+		// σε αυτόματη εύρεση των σχετικών παρτίδων. Η flag "kritiria" τίθεται
+		// TRUE εφόσον δόθηκαν κριτήρια στο URL.
+
+		$kritiria = FALSE;
+
+		// Δημιουργούμε javascript function η οποία καλείται ΑΦΟΥ σχηματιστεί
+		// η σελίδα επισκόπησης αρχειοθετημένων παρτίδων (ΣΕΑΠ). Στα input πεδία
+		// προσαρτούμε data που αφορούν στα κριτήρια που παρέχονται στο URL.
+
 		?>
 		<script type="text/javascript">
 		//<![CDATA[
 		Arxio.setupPost = function() {
-			var kritiria = false;
-
+			$('input').data('url', '');
 			<?php
 			if (Globals::perastike("pektis")) {
-				?>
-				Arxio.pektisInputDOM.data('url', '<?php print $_REQUEST["pektis"]; ?>');
-				Arxio.pektisInputDOM.val('<?php print $_REQUEST["pektis"]; ?>');
-				kritiria = true;
-				<?php
+				?>Arxio.pektisInputDOM.data('url', '<?php print $_REQUEST["pektis"]; ?>');<?php
+				$kritiria = TRUE;
 			}
+
+			// Αν δεν δόθηκε κριτήριο παίκτη στο URL, φροντίζουμε να θέσουμε
+			// κριτήριο που αφορά στον χρήστη που τρέχει το πρόγραμμα, εφόσον
+			// έχουμε επώνυμη χρήση. Σ' αυτή την περίπτωση όμως δεν προχωρούμε
+			// σε αυτόματη αναζήτηση.
+
 			else if (Globals::is_pektis()) {
-				?>
-				Arxio.pektisInputDOM.data('url', '<?php print $_SESSION["pektis"]; ?>');
-				Arxio.pektisInputDOM.val('<?php print $_SESSION["pektis"]; ?>');
-				<?php
-			}
-			else {
-				?>
-				Arxio.pektisInputDOM.data('url', '');
-				<?php
+				?>Arxio.pektisInputDOM.data('url', '<?php print $_SESSION["pektis"]; ?>');<?php
 			}
 
 			if (Globals::perastike("apo")) {
-				?>
-				Arxio.apoInputDOM.data('url', '<?php print $_REQUEST["apo"]; ?>');
-				Arxio.apoInputDOM.val('<?php print $_REQUEST["apo"]; ?>');
-				kritiria = true;
-				<?php
-			}
-			else {
-				?>
-				Arxio.apoInputDOM.data('url', '');
-				<?php
+				?>Arxio.apoInputDOM.data('url', '<?php print $_REQUEST["apo"]; ?>');<?php
+				$kritiria = TRUE;
 			}
 
 			if (Globals::perastike("eos")) {
-				?>
-				Arxio.eosInputDOM.data('url', '<?php print $_REQUEST["eos"]; ?>');
-				Arxio.eosInputDOM.val('<?php print $_REQUEST["eos"]; ?>');
-				kritiria = true;
-				<?php
-			}
-			else {
-				?>
-				Arxio.eosInputDOM.data('url', '');
-				<?php
+				?>Arxio.eosInputDOM.data('url', '<?php print $_REQUEST["eos"]; ?>');<?php
+				$kritiria = TRUE;
 			}
 
 			if (Globals::perastike("partida")) {
-				?>
-				Arxio.partidaInputDOM.data('url', '<?php print $_REQUEST["partida"]; ?>');
-				Arxio.partidaInputDOM.val('<?php print $_REQUEST["partida"]; ?>');
-				kritiria = true;
-				<?php
+				?>Arxio.partidaInputDOM.data('url', '<?php print $_REQUEST["partida"]; ?>');<?php
+				$kritiria = TRUE;
 			}
-			else {
-				?>
-				Arxio.partidaInputDOM.data('url', '');
-				<?php
+
+			// Προετοιμάζουμε την αναζήτηση, είτε έχουν δοθεί κριτήρια στο URL.
+			// είτε όχι.
+
+			?>Arxio.anazitisiReset();<?php
+
+			// Αν έχουν δοθεί κριτήρια στο URL, προχωρούμε σε αναζήτηση των
+			// σχετικών παρτίδων.
+
+			if ($kritiria) {
+				?>Arxio.goButtonDOM.trigger('click');<?php
 			}
 			?>
-
-			if (kritiria)
-			Arxio.goButtonDOM.trigger('click');
 		};
 		//]]>
 		</script>
