2ff5e9d5e35ff4522a6870a2200da75bbf652904
panos1962 <panos1962@localhost>
2015-02-17 10:54:19 +0200

    modifications

--- client/movie/panel.js
+++ client/movie/panel.js
@@ -41,6 +41,10 @@ console.log('STEP FORWARD: φάση', fasi, 'είδος', idos);
 		case 'ΑΛΛΑΓΗ':
 			if (idos === 'ΔΗΛΩΣΗ')
 			return Movie.pareTzogo();
+			break;
+		case 'ΠΛΗΡΩΜΗ':
+			Movie.trapezi.partidaReplay({eoske:Movie.dianomi.dianomiKodikosGet()});
+			break;
 		}
 
 		Movie.displayPartida();
@@ -67,6 +71,7 @@ Movie.panel.bpanelButtonPush(Movie.panel.energiaPrevButton = new PButton({
 			return;
 		}
 
+		Movie.trapezi.partidaReplay({eosxoris:Movie.dianomi.dianomiKodikosGet()});
 		for (i = 0; i <= Movie.energiaIndex; i++) {
 			energia = elist[i];
 			Movie.trapezi.trapeziProcessEnergia(energia);
