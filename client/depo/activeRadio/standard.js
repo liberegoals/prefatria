87ed087d75df3d7e07bc5b7f2c5df46ee46b48aa
panos1962 <panos1962@localhost>
2015-03-15 21:03:29 +0200

    modifications

--- client/depo/activeRadio/standard.js
+++ client/depo/activeRadio/standard.js
@@ -3,6 +3,10 @@ Diafimisi = {};
 $(document).ready(function() {
 	Diafimisi.kimeno1DOM = $('#kimeno1');
 	setTimeout(Diafimisi.display1, 1000);
+	$(document.body).
+	on('click', function() {
+		window.open('http://www.e-radio.gr/Active-Radio-Internet-Radio-i48/live', 'radiaki');
+	});
 });
 
 Diafimisi.display1 = function() {
